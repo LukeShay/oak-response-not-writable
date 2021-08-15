56696982950147afa8de73dd7adf92b1cc0c4c23
autoaim-jp <git@autoaim.jp>
2021-08-15 19:11:22 +0900

    [fix] using Promise prevents response-is-not-writable error

--- application.ts
+++ application.ts
@@ -1,22 +1,44 @@
 #! /usr/bin/env -S deno run --allow-net --allow-read --unstable
 
 import { Application, Router } from "https://deno.land/x/oak@v7.5.0/mod.ts";
-import { Handlebars } from "https://deno.land/x/handlebars@v0.7.0/mod.ts";
+import { delay } from 'https://deno.land/x/delay@v0.2.0/mod.ts';
 
 const app = new Application();
 const router = new Router();
-const handle = new Handlebars();
 
 router.get("/", function (ctx) {
   ctx.response.body = { hello: "There" };
 });
 
-router.get("/broken-view", async function (ctx) {
-  ctx.response.body = await handle.renderView("index");
+const getView = () => {
+  return new Promise(async (resolve, reject) => {
+    console.log('call busy function...')
+    await delay(3000)
+    console.log('done await')
+    const html = 'response with async/await/Promise after delay 3000ms'
+    resolve(html)
+  })
+}
+
+router.get("/view2", async (ctx) => {
+  const html: any = await getView();
+  ctx.response.body = html;
 });
 
+router.get("/another-view2", async (ctx) => {
+  const html: any = await new Promise(async (resolve, reject) => {
+    console.log('call busy function...')
+    await delay(3000)
+    console.log('done await')
+    const html = 'response with async/await/Promise after delay 3000ms'
+    resolve(html)
+  })
+  ctx.response.body = html;
+});
+
+
 router.get("/view", function (ctx) {
-  ctx.response.body = () => handle.renderView("index");
+  ctx.response.body = 'response without async/await/Promise';
 });
 
 app.use(router.routes(), router.allowedMethods());
